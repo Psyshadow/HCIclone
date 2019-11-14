bc4ab78963943bbc45d6d0419f557e3156281f79
BeatScherrer <beat.scherrer@gmail.com>
2019-11-14 13:46:19 +0100

    image location was fine, just need to commit the images... reverted

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -83,6 +83,3 @@ if ( !defined('ABSPATH') )
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
 
-/** Sets the wordpress upload directory */
-define( 'UPLOADS', ''.'images' );
-
